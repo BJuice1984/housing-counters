016b37a26d9b99af3ab03f2593e7132a490c368c
BJuice1984 <beetle_juice84@mail.ru>
2024-09-16 20:32:52 +0300

    add eslint

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const Title = styled.h1`
   margin: 0;
+  color: var(--color-text-main);
   font-family: 'Roboto', sans-serif;
   font-size: 24px;
   font-weight: 500;
